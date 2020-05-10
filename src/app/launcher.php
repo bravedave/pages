38b73c5189e4b78bcff8a4c4dff5ed69670a8f2e
Dave Bray <david@brayworth.com.au>
2020-05-10 16:22:27 +1000

    Update launcher.php

--- src/app/launcher.php
+++ src/app/launcher.php
@@ -17,23 +17,7 @@ class launcher {
 		$page = new page;	// from this namespace
 		$page->open();
 
-		if ( class_exists('Parsedown')) {
-			/**
-			 * Well not that simple - you have extended it with
-			 * composer require erusev/parsedown
-			 */
-
-			print Parsedown::instance()->text( file_get_contents( __DIR__ . '/../../Readme.md'));
-
-		}
-		else {
-
-			/**
-			 * Yeah - the Minimum Viable Product
-			 */
-			print '<h1>hello world</h1>';
-
-		}
+		print Parsedown::instance()->text( file_get_contents( __DIR__ . '/../../Readme.md'));
 
 		// page will self destruct and close
 
